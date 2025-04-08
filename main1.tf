21ac6bab17b230532047cc6df2ba2860be593081
Navathej <thota.navathej@capgemini.com>
2025-04-08 13:26:16 +0530

    updating

--- main1.tf
+++ main1.tf
@@ -143,5 +143,26 @@ resource "aws_instance" "ubuntu-server" {
 }
 */
 
+module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+
+  base_cidr_block = "10.0.0.0/22"
+  networks = [
+    {
+      name     = "module_network_a"
+      new_bits = 2
+    },
+    {
+      name     = "module_network_b"
+      new_bits = 2
+    },
+  ]
+}
+
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
+}
+
 
 #nat_gateway_id = aws_nat_gateway.nat_gateway.id
---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "hello-world" {
+  value       = "Hello world"
+  description = "print a hello world text output"
+}
+
+output "vpc_id" {
+  description = "output the ID for the primary vpc"
+  value       = aws_vpc.vpc.id
+}
+
+output "vpc_information" {
+  description = "VPC information about environment"
+  value       = "your ${aws_vpc.vpc.tags.environment}VPC has an ID of ${aws_vpc.vpc.id}"
+}
+
+output "public_url" {
+  description = "public URL for our web server"
+  value       = "paste ur url link of aws instance here"
+
+}
